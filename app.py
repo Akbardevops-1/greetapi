21cc733c3e869766e8e9211fffaac5390ae48686
Akbardevops-1 <97723410+Akbardevops-1@users.noreply.github.com>
2022-07-04 14:34:07 +0530

    create app.py for bootcamp

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/name')
+def index():
+    return jsonify({'name': 'akbar',
+                    'email': 'akbar@outlook.com'})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+app.run()
